a7a3ffb19fa45e6bdc6379bd9e716c6c041a0a01
ji.liu <ji.liu@convertlab.com>
2021-08-05 21:36:02 +0800

    chore: +eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+const eslintrc = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
+
+module.exports = eslintrc;
